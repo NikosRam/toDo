650caa98437fffe6c5f602d1dd344daaa65fae25
Nikolaos Ramantanis <nikosramantanis@hotmail.gr>
2023-03-17 00:43:48 +0200

    Initialise error handling mechanisms and handle undefined routes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 const toDoRouter = require("./routes/toDoRoutes");
 const userRouter = require("./routes/userRoutes");
 
@@ -13,5 +15,11 @@ app.use(express.json());
 
 app.use("/api/v1/todo", toDoRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`${req.originalUrl} endpoint not defined`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
