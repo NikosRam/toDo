b55fb7c94bda6c68c41d60626dd8aff333f98fa1
Nikolaos Ramantanis <nikosramantanis@hotmail.gr>
2023-03-17 01:28:28 +0200

    Refactor asynchronous functions error handling

--- backend/controllers/toDoController.js
+++ backend/controllers/toDoController.js
@@ -1,131 +1,97 @@
 const ToDo = require("../models/toDoModel");
 const APIFeatures = require("../utils/apiFeatures");
+const catchAsync = require("../utils/catchAsync");
 
 exports.aliasCompletedToDos = (req, res, next) => {
   req.query.completed = true;
   next();
 };
 
-exports.getAllToDos = async (req, res) => {
-  try {
-    const features = new APIFeatures(ToDo.find(), req.query)
-      .filter()
-      .sort()
-      .limit()
-      .paginate();
-    const toDos = await features.query;
+exports.getAllToDos = catchAsync(async (req, res, next) => {
+  const features = new APIFeatures(ToDo.find(), req.query)
+    .filter()
+    .sort()
+    .limit()
+    .paginate();
+  const toDos = await features.query;
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        toDos,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: "error",
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    data: {
+      toDos,
+    },
+  });
+});
 
-exports.createToDo = async (req, res) => {
-  try {
-    const newToDo = await ToDo.create(req.body);
+exports.createToDo = catchAsync(async (req, res, next) => {
+  const newToDo = await ToDo.create(req.body);
 
-    res.status(201).json({
-      status: "success",
-      data: {
-        toDo: newToDo,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+  res.status(201).json({
+    status: "success",
+    data: {
+      toDo: newToDo,
+    },
+  });
+});
 
-exports.updateToDo = async (req, res) => {
-  try {
-    const toDo = await ToDo.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+exports.updateToDo = catchAsync(async (req, res, next) => {
+  const toDo = await ToDo.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    res.status(200).json({
-      status: "success",
-      data: {
-        toDo,
-      },
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: "error",
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    data: {
+      toDo,
+    },
+  });
+});
 
-exports.deleteToDo = async (req, res) => {
-  try {
-    await ToDo.findByIdAndDelete(req.params.id);
+exports.deleteToDo = catchAsync(async (req, res, next) => {
+  await ToDo.findByIdAndDelete(req.params.id);
 
-    res.status(204).json({
-      status: "success",
-      data: null,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: "error",
-    });
-  }
-};
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
 
-exports.getCurrentDayToDos = async (req, res) => {
-  try {
-    const today = new Date();
+exports.getCurrentDayToDos = catchAsync(async (req, res, next) => {
+  const today = new Date();
 
-    const start = new Date(
-      today.getFullYear(),
-      today.getMonth(),
-      today.getDate(),
-      2,
-      0,
-      0,
-      0
-    );
-    const end = new Date(
-      today.getFullYear(),
-      today.getMonth(),
-      today.getDate(),
-      25,
-      59,
-      59,
-      999
-    );
+  const start = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate(),
+    0,
+    0,
+    0,
+    0
+  );
+  const end = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate(),
+    23,
+    59,
+    59,
+    999
+  );
 
-    const toDosToday = await ToDo.aggregate([
-      {
-        $match: {
-          date: {
-            $gte: start,
-            $lte: end,
-          },
+  const toDosToday = await ToDo.aggregate([
+    {
+      $match: {
+        date: {
+          $gte: start,
+          $lte: end,
         },
       },
-    ]);
+    },
+  ]);
 
-    res.status(200).json({
-      status: "success",
-      data: toDosToday,
-    });
-  } catch (err) {
-    res.status(404).json({
-      status: "fail",
-      message: "error",
-    });
-  }
-};
+  res.status(200).json({
+    status: "success",
+    data: toDosToday,
+  });
+});
